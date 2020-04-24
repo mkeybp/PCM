864ccdf38a3f93dbfef3ed920329e520ec53b0f3
Mads Oddershede Geertsen <mads40g4@student.eadania.dk>
2020-04-24 12:47:59 +0200

    MainMenu state

---
+++ PCM/State/MainMenuState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PCM.State
+{
+    public class MainMenuState : State
+    {
+        public MainMenuState(GameWorld gameWorld, GraphicsDevice graphicsDevice, ContentManager content) : base(gameWorld, graphicsDevice, content)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PCM/State/State.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCM.State
+{
+    public abstract class State
+    {
+        #region Fields
+        protected ContentManager stContent;
+
+        protected GameWorld stGameWorld;
+
+        protected GraphicsDevice stGraphics;
+
+        #endregion
+
+        #region Methods
+
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+
+        public abstract void PostUpdate(GameTime gameTime);
+
+        public State(GameWorld gameWorld, GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            stGameWorld = gameWorld;
+            stGraphics = graphicsDevice;
+            stContent = content;
+        }
+
+        public abstract void Update(GameTime gameTime);
+
+        #endregion
+    }
+}
